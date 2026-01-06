78381d48705d42ed4c0dddf86db4daa90c6a1aac
C. Raja Venkata Praney Teja <chunkupraney@gmail.com>
2026-01-06 10:12:48 +0530

    Initial

---
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(e ->
+            System.setProperty(e.getKey(), e.getValue())
+        );
+        
+	}
+
+}
